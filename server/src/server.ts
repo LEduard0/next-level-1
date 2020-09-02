208ef204c3b804790aa51cc3e950cc6a3905c6ec
Luiz Eduardo <luiz.eduardo@acct.global>
2020-09-01 22:29:14 -0300

    remove from server.ts and add a routes file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,37 +1,10 @@
 import express, { request } from "express";
+import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
 
-const users = ["Luiz", "Eduardo", "Queiroz"];
-
-app.get("/users", (request, response) => {
-  const search = String(request.query.search);
-
-  const filteredUsers = search
-    ? users.filter((user) => user.includes(search))
-    : users;
-
-  return response.json(filteredUsers);
-});
-
-app.get("/users/:id", (request, response) => {
-  const id = Number(request.params.id),
-    user = users[id];
-
-  return response.json(user);
-});
-
-app.post("/users", (req, res) => {
-  const data = req.body;
-
-  const user = {
-    name: data.name,
-    email: data.email,
-  };
-
-  return res.json(user);
-});
+app.use(routes);
 
 app.listen(3333);
