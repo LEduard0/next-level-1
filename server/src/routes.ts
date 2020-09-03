d8f05e0225123d48ce8a6fcc5b288777e9db54f6
Luiz Eduardo <leduardo152@gmail.com>
2020-09-02 22:24:39 -0300

    points table and relationship

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { request, response } from "express";
 import knex from "./database/connection";
 
 const routes = express.Router();
@@ -10,14 +10,50 @@ routes.get("/", (request, response) => {
 routes.get("/items", async (request, response) => {
   const items = await knex("items").select("*");
 
-  const serializedItems = items.map(item => {
+  const serializedItems = items.map((item) => {
     return {
       name: item.title,
-      image_url: `http://localhost:3333/uploads/${item.image}`, 
-    }
-  })
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+      id: item.id,
+    };
+  });
 
   return response.json(serializedItems);
+});
+
+routes.post("/points", async (request, response) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items,
+  } = request.body;
+
+  const ids = await knex("points").insert({
+    image: "img-fake",
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id: ids[0],
+    };
+  });
+
+  await knex("point_items").insert(pointItems);
+
+  return response.json({ success: true });
 });
 
 export default routes;
