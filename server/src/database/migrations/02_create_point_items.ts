a8019c8c9e0e97e1761f3cc71b91c89983cc4f1d
Luiz Eduardo <leduardo152@gmail.com>
2020-09-15 21:12:45 -0300

    create_items and create_point_items migration

--- server/src/database/migrations/02_create_point_items.ts
+++ server/src/database/migrations/02_create_point_items.ts
@@ -1,3 +1,15 @@
-export async function up() {}
+import Knex from "knex";
 
-export async function down() {}
+export async function up(knex: Knex) {
+  // up: cria a tabela
+  return knex.schema.createTable("point_items", (table) => {
+    table.increments("id").primary();
+    table.integer("point_id").notNullable().references("in").inTable("points");
+    table.integer("item_id").notNullable().references("in").inTable("items");
+  });
+}
+
+export async function down(knex: Knex) {
+  // down: deleta a tabela
+  return knex.schema.dropTable("point_items");
+}
