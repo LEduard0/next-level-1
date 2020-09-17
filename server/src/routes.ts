b6f57308a8baf41fb7b4d9be6f41cfd70b1d2959
Luiz Eduardo <leduardo152@gmail.com>
2020-09-16 23:11:32 -0300

    routes file update with postPoints and getItems

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,53 @@
 import express from "express";
+import knex from "./database/connection";
 
 const routes = express.Router();
 
-routes.get("/", (request, response) => {
-  return response.json(["Luiz", "Eduardo", "Queiroz"]);
+routes.get("/items", async (request, response) => {
+  const items = await knex("items").select("*");
+  const serializedItems = items.map((item) => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+  });
+
+  return response.json(serializedItems);
 });
 
-export default routes
+routes.post("/points", async (request, response) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items,
+  } = request.body;
+
+  const ids = await knex("points").insert({
+    image: "iamge-fake",
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id: ids[0],
+    };
+  });
+
+  await knex("point_items").insert(pointItems);
+
+  return response.json({ success: true });
+});
+
+export default routes;
