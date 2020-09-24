2a3ed82b861f2369924a375b847f393b6975653f
Luiz Eduardo <leduardo152@gmail.com>
2020-09-23 22:55:26 -0300

    first page home application

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
+import Home from "../src/pages/Home";
 import "./App.css";
 
-import Header from "../src/components/Header";
-
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  const handleButtonClick = () => {
-    setCounter(counter + 1);
-  };
-
   return (
     <div className="App">
-      <Header title="Hello" />
-      <h1>{counter}</h1>
-      <button type="button" onClick={handleButtonClick}>
-        Teste
-      </button>
+      <Home />
     </div>
   );
 }
