16045d97a0f3572f5c28da888d17bf9f3e465960
Luiz Eduardo <luiz.eduardo@acct.global>
2020-11-05 20:42:51 -0300

    add abortEarly

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -19,18 +19,21 @@ routes.get("/points/:id", pointsController.show);
 routes.post(
   "/points",
   upload.single("image"),
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().required(),
-      email: Joi.string().required(),
-      whatsapp: Joi.number().required(),
-      latitude: Joi.number().required(),
-      longitude: Joi.number().required(),
-      city: Joi.string().required(),
-      uf: Joi.string().required().max(2),
-      items: Joi.string().required(),
-    }),
-  }),
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required(),
+        whatsapp: Joi.number().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        items: Joi.string().required(),
+      }),
+    },
+    { abortEarly: false }
+  ),
   pointsController.create
 );
 
